32d3c9fa9e7599873c176a6b86913028fb95d4bc
Zuko Mgwili <zukomgwili@gmail.com>
2021-03-27 16:08:52 +0100

    refactor: update app.js to use app.html as the template

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import { Capsules } from "./capsules.js";
+import { readFile } from "./utils.js";
 class App extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
     }
 
-    connectedCallback(){
-        this.shadowRoot.innerHTML = Capsules.html;
+    async connectedCallback(){
+        this.shadowRoot.innerHTML = await readFile("./app.html");
+        this.shadowRoot.querySelector('main').innerHTML = Capsules.html;
     }
 }
 
