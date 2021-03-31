2a7556f2b2f7822e36f34b23e4a4f5559ef5f02b
Zuko Mgwili <zukomgwili@gmail.com>
2021-03-31 22:04:14 +0200

    refactor: simplify capsule component add capsules html page

--- capsule.js
+++ capsule.js
@@ -1,34 +1,46 @@
-import { readFile, createTemplate } from "./utils.js";
+const template = `
+<div>
+    <h1 id="serial"></h1>
+    <p id="details"></p>          
+</div>
+`;
+
+const styles = `<style>
+h1{
+    color:#d34d89;
+}
+</style>`;
 
 export class Capsule extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'open' })
+            .appendChild(document.createRange()
+                .createContextualFragment(styles + template));
     }
 
-    async connectedCallback() {
-        const css = await readFile("./capsule.css");
-        const html = await readFile("./capsule.html");
-        const data = {
-            'serial': this.getAttribute('serial'),
-            'details': this.getAttribute('details')
-        };
-        const content = createTemplate(css, html, data);
-        this.shadowRoot.innerHTML = content;
+    connectedCallback() {
         this.h1 = this.shadowRoot.querySelector('h1');
         this.h1.addEventListener('click', this.showDetail);
-
     }
 
     disconnectedCallback() {
         this.h1.addEventListener('click', this.showDetail);
     }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.shadowRoot.querySelector(`#${name}`).textContent = newValue;
+    }
+
+    static get observedAttributes() {
+        return ["serial", "details"];
+    }
+
     showDetail() {
         console.log("Hello!!")
     }
 
-    static getHtml({serial, details}){
+    static getHtml({ serial, details }) {
         return `<v-capsule serial="${serial}" details="${details}"></v-capsule>`;
     }
 
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import "./utils.js"
-import "./capsule.js";
-import "./capsules.js";
-import "./app.js";
+class Index extends HTMLElement {
+    constructor() {
+        super();
+        const { content } = document.querySelector("#index");
+        this.attachShadow({ mode: 'open' }).appendChild(content.cloneNode(true));
+    }
+}
 
+customElements.define('v-index', Index);
